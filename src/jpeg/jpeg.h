6a898d44a7a633838018f2df9bcc480cf3302678
kalaider <a.kalaider@yandex.ru>
2018-09-23 18:46:30 +0300

    build: add project

---
+++ src/jpeg/jpeg.h
@@ -0,0 +1,30 @@
+// jpeg.h : main header file for the PROJECT_NAME application
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+    #error "include 'stdafx.h' before including this file for PCH"
+#endif
+
+#include "resource.h"        // main symbols
+
+// CJpegApp:
+// See jpeg.cpp for the implementation of this class
+//
+
+class CJpegApp : public CWinApp
+{
+public:
+    CJpegApp();
+
+// Overrides
+public:
+    virtual BOOL InitInstance();
+
+// Implementation
+
+    DECLARE_MESSAGE_MAP()
+};
+
+extern CJpegApp theApp;
